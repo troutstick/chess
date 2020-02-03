3c0d807ef206b8b584ea957a4ce80d69d993c400
Justin Thein <jthein@berkeley.edu>
2020-02-03 00:56:35 -0800

    player simulation partially added

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import board
 import players
 
-c = board.Chessboard  # TEMPORARY: for easier access
-a = c()
+class Game:
+    """The thing we're playing. Basically the main loop."""
+    def __init__(self):
+        self.board = board.Chessboard()
+        self.white = self.board.white
+        self.black = self.board.black
+        self.turn = 0
+
+    def sim(self):
+        try:
+            self.white.play()
+            self.black.play()
+            self.turn += 1
+        except GameOverException:
+            print("Game over!")
+
+class GameOverException(Exception):
+    pass
+
+g = Game()
+
+a = g.board  # TEMPORARY: for easier access
 a
 n = a.list_pieces(True)[1]
 r = a.list_pieces(True)[0]
@@ -10,3 +30,4 @@ p = a.list_pieces(True)[10]
 print(a.legal_moves(n))
 print(a.legal_moves(r))
 print(a.legal_moves(p))
+g.sim()
--- players.py
+++ players.py
@@ -5,6 +5,20 @@ class Player:
     
     Handles move legality, turns, piece movement, and basically the things that the players do.
     """
-    def __init__(self, chessboard, is_white):
+    def __init__(self, board, is_white):
         self.is_white = is_white
-        self.chessboard = chessboard
+        self.board = board
+
+    def play(self):
+        """Make a move."""
+        self.board.list_pieces(self.is_white)
+        print(self.board)
+        legal_moves = self.board.all_moves(self.is_white)
+        print(legal_moves)
+        print("You may move these pieces:")
+        piece_str = ""
+        for piece_list in legal_moves:
+            if len(piece_list) > 1:
+                piece_str += (str(piece_list[0]) + " ")
+        print(piece_str)
+        
